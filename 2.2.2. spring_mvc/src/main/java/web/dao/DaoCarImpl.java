0f4488f63d68032e3a2138c493942a96a1c08a2b
SergeyOvsyannikov1998 <87448596+SergeyOvsyannikov1998@users.noreply.github.com>
2021-08-03 16:20:26 +0300

    готово

--- 2.2.2. spring_mvc/src/main/java/web/dao/DaoCarImpl.java
+++ 2.2.2. spring_mvc/src/main/java/web/dao/DaoCarImpl.java
@@ -4,6 +4,7 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DaoCarImpl implements DaoCar{
     private final List<Car> list;
@@ -14,16 +15,14 @@ public class DaoCarImpl implements DaoCar{
         list.add(new Car("Vladimir", "Nissan", "3"));
         list.add(new Car("Dmitriy", "Lada", "4"));
         list.add(new Car("Nikolay", "Toyota", "5"));
-    };
+    }
     @Override
     public List<Car> getObj(int count) {
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i <= count; i++){
-            if (i>=count||i>=5){
-                return cars;
-            }
-            cars.add(list.get(i));
+        if (count>=5){
+            return list;
+        } else {
+            List<Car> doubleList = list.stream().limit(count).collect(Collectors.toList());
+            return doubleList;
         }
-        return cars;
     }
 }
